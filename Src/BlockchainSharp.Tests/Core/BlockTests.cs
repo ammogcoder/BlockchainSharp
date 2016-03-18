c44a1b958c5cbe47adbeb57be70b566869b007ae
ajlopez <webmaster@ajlopez.com>
2016-03-18 05:55:05 -0300

    Fixing source analysis

--- Src/BlockchainSharp.Tests/Core/BlockTests.cs
+++ Src/BlockchainSharp.Tests/Core/BlockTests.cs
@@ -1,11 +1,11 @@
 ﻿namespace BlockchainSharp.Tests.Core
 {
     using System;
-    using System.Text;
     using System.Collections.Generic;
     using System.Linq;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Text;
     using BlockchainSharp.Core;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
     public class BlockTests
