c42712b529049831077a033c30b7919e3ff13ac0
ajlopez <webmaster@ajlopez.com>
2016-03-18 05:52:23 -0300

    Create block with number

---
+++ Src/BlockchainSharp.Tests/Core/BlockTests.cs
@@ -0,0 +1,21 @@
+﻿namespace BlockchainSharp.Tests.Core
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BlockchainSharp.Core;
+
+    [TestClass]
+    public class BlockTests
+    {
+        [TestMethod]
+        public void CreateWithNumber()
+        {
+            Block block = new Block(42);
+
+            Assert.AreEqual(42, block.Number);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Core/Block.cs
@@ -0,0 +1,19 @@
+﻿namespace BlockchainSharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Block
+    {
+        private long number;
+
+        public Block(long number)
+        {
+            this.number = number;
+        }
+
+        public long Number { get { return this.number; } }
+    }
+}
