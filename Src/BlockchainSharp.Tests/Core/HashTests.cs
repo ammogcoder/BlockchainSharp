206df877edaaf0b69eb70c2d78185a446cec6192
ajlopez <webmaster@ajlopez.com>
2016-03-22 07:32:53 -0300

    Hash first creation test

---
+++ Src/BlockchainSharp.Tests/Core/HashTests.cs
@@ -0,0 +1,22 @@
+﻿namespace BlockchainSharp.Tests.Core
+{
+    using System;
+    using System.Linq;
+    using BlockchainSharp.Core;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class HashTests
+    {
+        [TestMethod]
+        public void CreateWithBytes()
+        {
+            Hash hash = new Hash();
+
+            Assert.IsNotNull(hash.Bytes);
+            Assert.AreEqual(20, hash.Bytes.Length);
+            Assert.IsTrue(hash.Bytes.Any(b => b != 0x00));
+            Assert.IsTrue(hash.Bytes.Any(b => b != 0xff));
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Core/Hash.cs
@@ -0,0 +1,21 @@
+﻿namespace BlockchainSharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Hash
+    {
+        private static Random random = new Random();
+        private byte[] bytes;
+
+        public Hash()
+        {
+            this.bytes = new byte[20];
+            random.NextBytes(this.bytes);
+        }
+
+        public byte[] Bytes { get { return this.bytes; } }
+    }
+}
