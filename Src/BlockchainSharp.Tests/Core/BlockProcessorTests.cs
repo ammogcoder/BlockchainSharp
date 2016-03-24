226bb54785edbb71b66114403f789659a7122b5a
ajlopez <webmaster@ajlopez.com>
2016-03-24 13:51:25 -0300

    BlockProcessor first tests

---
+++ Src/BlockchainSharp.Tests/Core/BlockProcessorTests.cs
@@ -0,0 +1,51 @@
+﻿namespace BlockchainSharp.Tests.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BlockchainSharp.Core;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BlockProcessorTests
+    {
+        [TestMethod]
+        public void CreateWithNoBlockChain()
+        {
+            BlockProcessor processor = new BlockProcessor();
+
+            Assert.IsNull(processor.BlockChain);
+        }
+
+        [TestMethod]
+        public void ProcessGenesisBlock()
+        {
+            Block genesis = new Block(0, null);
+            BlockProcessor processor = new BlockProcessor();
+
+            processor.Process(genesis);
+
+            Assert.IsNotNull(processor.BlockChain);
+            Assert.AreEqual(0, processor.BlockChain.BestBlockNumber);
+            Assert.AreEqual(genesis, processor.BlockChain.GetBlock(0));
+        }
+
+        [TestMethod]
+        public void ProcessTwoBlocks()
+        {
+            Block genesis = new Block(0, null);
+            Block block = new Block(1, genesis.Hash);
+
+            BlockProcessor processor = new BlockProcessor();
+
+            processor.Process(genesis);
+            processor.Process(block);
+
+            Assert.IsNotNull(processor.BlockChain);
+            Assert.AreEqual(1, processor.BlockChain.BestBlockNumber);
+            Assert.AreEqual(genesis, processor.BlockChain.GetBlock(0));
+            Assert.AreEqual(block, processor.BlockChain.GetBlock(1));
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Core/BlockProcessor.cs
@@ -0,0 +1,22 @@
+﻿namespace BlockchainSharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class BlockProcessor
+    {
+        private BlockChain chain;
+
+        public BlockChain BlockChain { get { return this.chain; } }
+
+        public void Process(Block block)
+        {
+            if (this.chain == null)
+                this.chain = new BlockChain(block);
+            else
+                this.chain.TryToAdd(block);
+        }
+    }
+}
