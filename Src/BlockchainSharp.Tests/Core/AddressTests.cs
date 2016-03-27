19c5cac4eab256ee5ae2495dd2b35813eb30a7d5
ajlopez <webmaster@ajlopez.com>
2016-03-27 10:36:32 -0300

    Address first tests

---
+++ Src/BlockchainSharp.Tests/Core/AddressTests.cs
@@ -0,0 +1,53 @@
+﻿namespace BlockchainSharp.Tests.Core
+{
+    using System;
+    using System.Linq;
+    using BlockchainSharp.Core;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AddressTests
+    {
+        [TestMethod]
+        public void CreateWithRandomBytes()
+        {
+            Address address = new Address();
+
+            Assert.IsNotNull(address.Bytes);
+            Assert.AreEqual(20, address.Bytes.Length);
+            Assert.IsTrue(address.Bytes.Any(b => b != 0x00));
+            Assert.IsTrue(address.Bytes.Any(b => b != 0xff));
+        }
+
+        [TestMethod]
+        public void CreateWithBytes()
+        {
+            Address address = new Address(new byte[] { 0x00, 0x01, 0x02, 0x03 });
+
+            Assert.IsNotNull(address.Bytes);
+            Assert.AreEqual(4, address.Bytes.Length);
+
+            for (int k = 0; k < address.Bytes.Length; k++)
+                Assert.AreEqual((byte)k, address.Bytes[k]);
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            Address address1 = new Address(new byte[] { 0x00, 0x01, 0x02, 0x03 });
+            Address address2 = new Address(new byte[] { 0x00, 0x01, 0x02, 0x03, 0x00 });
+            Address address3 = new Address(new byte[] { 0x00, 0x01, 0x02, 0x02 });
+            Address address4 = new Address(new byte[] { 0x00, 0x01, 0x02, 0x03 });
+
+            Assert.IsTrue(address1.Equals(address1));
+            Assert.IsTrue(address1.Equals(address4));
+            Assert.IsFalse(address1.Equals(address2));
+            Assert.IsFalse(address1.Equals(address3));
+            Assert.IsFalse(address1.Equals(null));
+            Assert.IsFalse(address1.Equals(42));
+            Assert.IsFalse(address1.Equals("foo"));
+
+            Assert.AreEqual(address1.GetHashCode(), address4.GetHashCode());
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Core/Address.cs
@@ -0,0 +1,62 @@
+﻿namespace BlockchainSharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Address
+    {
+        private static Random random = new Random();
+        private byte[] bytes;
+
+        public Address()
+        {
+            this.bytes = new byte[20];
+            random.NextBytes(this.bytes);
+        }
+
+        public Address(byte[] bytes)
+        {
+            this.bytes = bytes;
+        }
+
+        public byte[] Bytes { get { return this.bytes; } }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (this == obj)
+                return true;
+
+            if (!(obj is Address))
+                return false;
+
+            Address h = (Address)obj;
+
+            if (this.bytes.Length != h.bytes.Length)
+                return false;
+
+            for (int k = 0; k < this.bytes.Length; k++)
+                if (this.bytes[k] != h.bytes[k])
+                    return false;
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            int value = 0;
+
+            for (int k = 0; k < this.bytes.Length; k++)
+            {
+                value += this.bytes[k];
+                value <<= 1;
+            }
+
+            return value;
+        }
+    }
+}
