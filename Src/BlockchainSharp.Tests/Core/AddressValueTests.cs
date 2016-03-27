22e7f07258c336af16acf790d9a16deb5d012db4
ajlopez <webmaster@ajlopez.com>
2016-03-27 10:45:03 -0300

    AddressValue first creation test

---
+++ Src/BlockchainSharp.Tests/Core/AddressValueTests.cs
@@ -0,0 +1,23 @@
+﻿namespace BlockchainSharp.Tests.Core
+{
+    using System;
+    using System.Numerics;
+    using BlockchainSharp.Core;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AddressValueTests
+    {
+        [TestMethod]
+        public void CreateWithAddressAndValue()
+        {
+            Address address = new Address();
+            BigInteger value = new BigInteger(100);
+
+            var av = new AddressValue(address, value);
+
+            Assert.AreEqual(address, av.Address);
+            Assert.AreEqual(value, av.Value);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Core/AddressValue.cs
@@ -0,0 +1,24 @@
+﻿namespace BlockchainSharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+
+    public class AddressValue
+    {
+        private Address address;
+        private BigInteger value;
+
+        public AddressValue(Address address, BigInteger value)
+        {
+            this.address = address;
+            this.value = value;
+        }
+
+        public Address Address { get { return this.address; } }
+
+        public BigInteger Value { get { return this.value; } }
+    }
+}
