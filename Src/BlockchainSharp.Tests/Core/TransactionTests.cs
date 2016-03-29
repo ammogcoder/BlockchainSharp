8e347b519661c6edcf1f4797e59c8a4b8bdee072
ajlopez <webmaster@ajlopez.com>
2016-03-29 09:20:55 -0300

    Transaction creation first test

---
+++ Src/BlockchainSharp.Tests/Core/TransactionTests.cs
@@ -0,0 +1,25 @@
+﻿namespace BlockchainSharp.Tests.Core
+{
+    using System;
+    using System.Numerics;
+    using BlockchainSharp.Core;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TransactionTests
+    {
+        [TestMethod]
+        public void CreateTransactionWithTotals()
+        {
+            Transaction transaction = new Transaction(new AddressValue[] {
+                new AddressValue(new Address(), new BigInteger(100))
+            }, new AddressValue[] {
+                new AddressValue(new Address(), new BigInteger(50)),
+                new AddressValue(new Address(), new BigInteger(40))
+            });
+
+            Assert.AreEqual(new BigInteger(100), transaction.TotalFrom);
+            Assert.AreEqual(new BigInteger(90), transaction.TotalTo);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Core/Transaction.cs
@@ -0,0 +1,42 @@
+﻿namespace BlockchainSharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+
+    public class Transaction
+    {
+        private IList<AddressValue> from;
+        private IList<AddressValue> to;
+
+        public Transaction(IEnumerable<AddressValue> from, IEnumerable<AddressValue> to)
+        {
+            this.from = new List<AddressValue>(from);
+            this.to = new List<AddressValue>(to);
+        }
+
+        public BigInteger TotalFrom { get {
+            BigInteger result = BigInteger.Zero;
+
+            foreach (var av in this.from)
+                result = BigInteger.Add(result, av.Value);
+
+            return result;
+        } }
+
+        public BigInteger TotalTo
+        {
+            get
+            {
+                BigInteger result = BigInteger.Zero;
+
+                foreach (var av in this.to)
+                    result = BigInteger.Add(result, av.Value);
+
+                return result;
+            }
+        }
+    }
+}
