3a4d9d93ae315bb92a8165b0246c43dc08ca441d
ajlopez <webmaster@ajlopez.com>
2016-04-03 12:03:18 -0300

    Rlp first single low byte test

---
+++ Src/BlockchainSharp.Tests/Encoding/RlpTests.cs
@@ -0,0 +1,20 @@
+﻿namespace BlockchainSharp.Tests.Encoding
+{
+    using System;
+    using BlockchainSharp.Encoding;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RlpTests
+    {
+        [TestMethod]
+        public void EncodeSingleLowByte()
+        {
+            var result = Rlp.Encode(new byte[] { 0x01 });
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Length);
+            Assert.AreEqual(0x01, result[0]);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Encoding/Rlp.cs
@@ -0,0 +1,15 @@
+﻿namespace BlockchainSharp.Encoding
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public static class Rlp
+    {
+        public static byte[] Encode(byte[] bytes)
+        {
+            return bytes;
+        }
+    }
+}
