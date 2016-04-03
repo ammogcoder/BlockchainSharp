9d74efec6cefbc3f8298bdc514c0bd25892e096e
ajlopez <webmaster@ajlopez.com>
2016-04-03 10:50:57 -0300

    Trie get unknown key

---
+++ Src/BlockchainSharp.Tests/Tries/TrieTests.cs
@@ -0,0 +1,18 @@
+﻿namespace BlockchainSharp.Tests.Tries
+{
+    using System;
+    using BlockchainSharp.Tries;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TrieTests
+    {
+        [TestMethod]
+        public void GetUnknowKey()
+        {
+            Trie<string> trie = new Trie<string>();
+
+            Assert.IsNull(trie.Get("foo"));
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Tries/Trie.cs
@@ -0,0 +1,26 @@
+﻿namespace BlockchainSharp.Tries
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Trie<T>
+    {
+        private object[] leafs;
+
+        public Trie() 
+        {
+            this.leafs = new object[16];
+        }
+
+        public void Put(string key, T value)
+        {
+        }
+
+        public T Get(string key)
+        {
+            return default(T);
+        }
+    }
+}
