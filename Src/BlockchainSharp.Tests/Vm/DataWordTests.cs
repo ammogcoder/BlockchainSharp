a6f1ff1a65c44762addb0dc16c718f0ba8cf1fb5
ajlopez <webmaster@ajlopez.com>
2016-04-16 06:36:12 -0300

    DataWord first implementation and tests, borrowed from EthSharp project

---
+++ Src/BlockchainSharp.Tests/Vm/DataWordTests.cs
@@ -0,0 +1,167 @@
+﻿namespace BlockchainSharp.Tests.Vm
+{
+    using System;
+    using System.Numerics;
+    using BlockchainSharp.Vm;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DataWordTests
+    {
+        [TestMethod]
+        public void CreateDataWordUsingPositiveInteger()
+        {
+            var dw = new DataWord(1);
+
+            var result = dw.Data;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(32, result.Length);
+
+            for (int k = 0; k < 31; k++)
+                Assert.AreEqual(0x00, result[k]);
+
+            Assert.AreEqual(0x01, result[31]);
+        }
+
+        [TestMethod]
+        public void CreateDataWordUsingBytes()
+        {
+            var dw = new DataWord(new byte[] { 0x01, 0x02 });
+
+            Assert.AreEqual(new BigInteger(258), dw.Value);
+        }
+
+        [TestMethod]
+        public void CreateNegativeDataWordUsingBytes()
+        {
+            var dw = new DataWord(new byte[] { 0xff, 0xff });
+
+            Assert.AreEqual(new BigInteger(-1), dw.Value);
+        }
+
+        [TestMethod]
+        public void CreateDataWordUsingNegativeInteger()
+        {
+            var dw = new DataWord(-1);
+
+            var result = dw.Data;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(32, result.Length);
+
+            for (int k = 0; k < 32; k++)
+                Assert.AreEqual(0xff, result[k]);
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            var dw1 = new DataWord(1);
+            var dw2 = new DataWord(2);
+            var dw1b = new DataWord(1);
+
+            Assert.AreEqual(dw1, dw1);
+            Assert.AreEqual(dw1, dw1b);
+            Assert.AreEqual(dw1b, dw1);
+
+            Assert.IsFalse(dw1.Equals(null));
+
+            Assert.AreNotEqual(dw1, null);
+            Assert.AreNotEqual(dw1, "foo");
+            Assert.AreNotEqual(dw1, 42);
+            Assert.AreNotEqual(dw1, dw2);
+
+            Assert.AreEqual(dw1.GetHashCode(), dw1b.GetHashCode());
+        }
+
+        [TestMethod]
+        public void NegatePositiveInteger()
+        {
+            var dw = new DataWord(3);
+
+            var result = dw.Negate();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(new DataWord(-3), result);
+        }
+
+        [TestMethod]
+        public void GetSmallIntegerAsBigInteger()
+        {
+            var dw = new DataWord(1);
+
+            var result = dw.Value;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(BigInteger.One, result);
+        }
+
+        [TestMethod]
+        public void AddOneToOne()
+        {
+            var dw = new DataWord(1);
+
+            var result = dw.Add(dw).Value;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(new BigInteger(2), result);
+        }
+
+        [TestMethod]
+        public void AddOneToMinusOne()
+        {
+            var dw = new DataWord(1);
+            var dw2 = new DataWord(-1);
+
+            var result = dw.Add(dw2).Value;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(BigInteger.Zero, result);
+        }
+
+        [TestMethod]
+        public void SubtractOneFromTwo()
+        {
+            var dw = new DataWord(2);
+
+            var result = dw.Subtract(new DataWord(1)).Value;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(BigInteger.One, result);
+        }
+
+        [TestMethod]
+        public void SubtractMinusOneFromTwo()
+        {
+            var dw = new DataWord(2);
+
+            var result = dw.Subtract(new DataWord(-1)).Value;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(new BigInteger(3), result);
+        }
+
+        [TestMethod]
+        public void MultiplyTwoByThree()
+        {
+            var dw = new DataWord(2);
+
+            var result = dw.Multiply(new DataWord(3)).Value;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(new BigInteger(6), result);
+        }
+
+        [TestMethod]
+        public void DivideSixIntoThree()
+        {
+            var dw = new DataWord(6);
+
+            var result = dw.Divide(new DataWord(3)).Value;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(new BigInteger(2), result);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Vm/DataWord.cs
@@ -0,0 +1,102 @@
+﻿namespace BlockchainSharp.Vm
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+
+    public class DataWord
+    {
+        private byte[] data;
+
+        public DataWord(int num)
+        {
+            this.data = new byte[32];
+            int value = num;
+
+            for (int k = 0; k < 4; k++)
+            {
+                this.data[k] = (byte)(value & 0x00ff);
+                value >>= 8;
+            }
+
+            if (num < 0)
+                for (int k = 4; k < 32; k++)
+                    this.data[k] = (byte)0xff;
+        }
+
+        public DataWord(byte[] bytes)
+        {
+            this.data = new byte[32];
+
+            for (int k = 0; k < bytes.Length; k++)
+                this.data[k] = bytes[bytes.Length - k - 1];
+
+            if ((bytes[0] & 0x80) != 0)
+                for (int k = bytes.Length; k < 32; k++)
+                    this.data[k] = 0xff;
+        }
+
+        private DataWord(byte[] bytes, bool inverse)
+        {
+            this.data = new byte[32];
+
+            Array.Copy(bytes, 0, this.data, 0, bytes.Length);
+
+            if ((bytes[bytes.Length - 1] & 0x80) != 0)
+                for (int k = bytes.Length; k < 32; k++)
+                    this.data[k] = 0xff;
+        }
+
+        public DataWord(BigInteger value)
+            : this(value.ToByteArray(), true)
+        {
+        }
+
+        public byte[] Data { get { return this.data.Reverse().ToArray(); } }
+
+        public BigInteger Value { get { return new BigInteger(this.data); } }
+
+        public DataWord Negate()
+        {
+            return new DataWord(BigInteger.Negate(this.Value));
+        }
+
+        public DataWord Add(DataWord dw)
+        {
+            return new DataWord(BigInteger.Add(this.Value, dw.Value));
+        }
+
+        public DataWord Subtract(DataWord dw)
+        {
+            return new DataWord(BigInteger.Subtract(this.Value, dw.Value));
+        }
+
+        public DataWord Multiply(DataWord dw)
+        {
+            return new DataWord(BigInteger.Multiply(this.Value, dw.Value));
+        }
+
+        public DataWord Divide(DataWord dw)
+        {
+            return new DataWord(BigInteger.Divide(this.Value, dw.Value));
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Value.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (!(obj is DataWord))
+                return false;
+
+            return this.Value.Equals(((DataWord)obj).Value);
+        }
+    }
+}
