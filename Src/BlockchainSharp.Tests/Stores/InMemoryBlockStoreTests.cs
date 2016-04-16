6590a1149769ab1246fc17f95e377768e8cab846
ajlopez <webmaster@ajlopez.com>
2016-04-16 18:32:36 -0300

    InMemoryBlockStore GetByHash undefined block

---
+++ Src/BlockchainSharp.Tests/Stores/InMemoryBlockStoreTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BlockchainSharp.Stores;
+using BlockchainSharp.Core;
+
+namespace BlockchainSharp.Tests.Stores
+{
+    [TestClass]
+    public class InMemoryBlockStoreTests
+    {
+        [TestMethod]
+        public void GetUndefinedBlockByHash()
+        {
+            var store = new InMemoryBlockStore();
+
+            Assert.IsNull(store.GetByHash(new Hash()));
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Stores/InMemoryBlockStore.cs
@@ -0,0 +1,16 @@
+﻿namespace BlockchainSharp.Stores
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BlockchainSharp.Core;
+
+    public class InMemoryBlockStore
+    {
+        public Block GetByHash(Hash hash)
+        {
+            return null;
+        }
+    }
+}
