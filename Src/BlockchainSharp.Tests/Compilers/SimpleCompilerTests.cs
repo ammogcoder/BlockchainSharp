4c5959c808809fce9d11955e58f2ab6208c156d0
ajlopez <webmaster@ajlopez.com>
2016-04-17 10:28:11 -0300

    SimpleCompiler compiles first arithmetic ops

--- Src/BlockchainSharp.Tests/Compilers/SimpleCompilerTests.cs
+++ Src/BlockchainSharp.Tests/Compilers/SimpleCompilerTests.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using BlockchainSharp.Compilers;
+    using BlockchainSharp.Vm;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
@@ -27,6 +28,37 @@
 
             Assert.IsNotNull(result);
             Assert.AreEqual(0, result.Length);
+        }
+
+        [TestMethod]
+        public void CompileArithmeticOperations()
+        {
+            CompileBytecode("add", Bytecodes.Add);
+            CompileBytecode("subtract", Bytecodes.Subtract);
+            CompileBytecode("multiply", Bytecodes.Multiply);
+            CompileBytecode("divide", Bytecodes.Divide);
+        }
+
+        [TestMethod]
+        public void CompileSkippingCommentsAndEmptyLines()
+        {
+            CompileBytecode("add\n", Bytecodes.Add);
+            CompileBytecode("add\r\n", Bytecodes.Add);
+            CompileBytecode("add\r", Bytecodes.Add);
+            CompileBytecode("# comment\nadd\n", Bytecodes.Add);
+            CompileBytecode("\n\nadd # comment\n", Bytecodes.Add);
+            CompileBytecode("\r\n\r\nadd # comment\n", Bytecodes.Add);
+            CompileBytecode("\r\radd # comment\n", Bytecodes.Add);
+        }
+
+        private static void CompileBytecode(string text, Bytecodes bc)
+        {
+            var compiler = new SimpleCompiler(text);
+            var result = compiler.Compile();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Length);
+            Assert.AreEqual((byte)bc, result[0]);
         }
     }
 }
--- Src/BlockchainSharp/Compilers/SimpleCompiler.cs
+++ Src/BlockchainSharp/Compilers/SimpleCompiler.cs
@@ -9,14 +9,56 @@
     public class SimpleCompiler
     {
         private BytecodeCompiler compiler = new BytecodeCompiler();
+        private string[] lines;
+        private int nlines;
+        private int cline;
 
         public SimpleCompiler(string text)
         {
+            if (text == null)
+                this.lines = new string[0];
+            else
+                this.lines = text.Split('\n', '\r');
+
+            this.nlines = this.lines.Length;
+            this.cline = 0;
         }
 
         public byte[] Compile()
         {
+            while (this.cline < this.nlines)
+                this.ProcessLine(this.lines[this.cline++]);
+
             return compiler.ToBytes();
+        }
+
+        private void ProcessLine(string line)
+        {
+            int p = line.IndexOf("#");
+
+            if (p >= 0)
+                line = line.Substring(0, p);
+
+            line = line.Trim();
+
+            if (line.Length == 0)
+                return;
+
+            var words = line.Split(' ', '\t');
+
+            if (words.Length == 0)
+                return;
+
+            var verb = words[0].ToLower();
+
+            if (verb == "add")
+                this.compiler.Add();
+            else if (verb == "subtract")
+                this.compiler.Subtract();
+            else if (verb == "multiply")
+                this.compiler.Multiply();
+            else if (verb == "divide")
+                this.compiler.Divide();
         }
     }
 }
