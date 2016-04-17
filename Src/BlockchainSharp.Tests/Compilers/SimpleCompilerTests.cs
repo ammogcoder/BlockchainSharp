4cad7476847b543502b16aac65c953de141f584e
ajlopez <webmaster@ajlopez.com>
2016-04-17 10:14:46 -0300

    SimpleCompiler compiles empty string to empty byte array

---
+++ Src/BlockchainSharp.Tests/Compilers/SimpleCompilerTests.cs
@@ -0,0 +1,21 @@
+﻿namespace BlockchainSharp.Tests.Compilers
+{
+    using System;
+    using BlockchainSharp.Compilers;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SimpleCompilerTests
+    {
+        [TestMethod]
+        public void CompileEmptyString()
+        {
+            var compiler = new SimpleCompiler(string.Empty);
+
+            var result = compiler.Compile();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result.Length);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Compilers/SimpleCompiler.cs
@@ -0,0 +1,19 @@
+﻿namespace BlockchainSharp.Compilers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class SimpleCompiler
+    {
+        public SimpleCompiler(string text)
+        {
+        }
+
+        public byte[] Compile()
+        {
+            return new byte[0];
+        }
+    }
+}
