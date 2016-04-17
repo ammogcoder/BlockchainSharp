90fe66bedb14e61de98b785077b597dd12d8504a
ajlopez <webmaster@ajlopez.com>
2016-04-17 10:17:09 -0300

    SimpleCompiler internal refactor

--- Src/BlockchainSharp/Compilers/SimpleCompiler.cs
+++ Src/BlockchainSharp/Compilers/SimpleCompiler.cs
@@ -4,16 +4,19 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using BlockchainSharp.Vm;
 
     public class SimpleCompiler
     {
+        private BytecodeCompiler compiler = new BytecodeCompiler();
+
         public SimpleCompiler(string text)
         {
         }
 
         public byte[] Compile()
         {
-            return new byte[0];
+            return compiler.ToBytes();
         }
     }
 }
