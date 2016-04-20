bf4bd8154b1f16da003d2e423506b3bd860b2b4a
ajlopez <webmaster@ajlopez.com>
2016-04-20 07:55:42 -0300

    Refactor extract BlockStore interface:

---
+++ Src/BlockchainSharp/Stores/BlockStore.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BlockchainSharp.Stores
+{
+    interface BlockStore
+    {
+        BlockchainSharp.Core.Block GetByHash(BlockchainSharp.Core.Hash hash);
+        System.Collections.Generic.IEnumerable<BlockchainSharp.Core.Block> GetByNumber(long number);
+        void Save(BlockchainSharp.Core.Block block);
+    }
+}
