4a079bdbd34cac3facd2d6efa2bee8f66830b7a5
ajlopez <webmaster@ajlopez.com>
2016-04-23 10:15:10 -0300

    Create AccountState with balance

---
+++ Src/BlockchainSharp.Tests/Core/AccountStateTests.cs
@@ -0,0 +1,19 @@
+﻿namespace BlockchainSharp.Tests.Core
+{
+    using System;
+    using System.Numerics;
+    using BlockchainSharp.Core;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AccountStateTests
+    {
+        [TestMethod]
+        public void CreateWithBalance()
+        {
+            var state = new AccountState(BigInteger.One);
+
+            Assert.AreEqual(BigInteger.One, state.Balance);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Core/AccountState.cs
@@ -0,0 +1,20 @@
+﻿namespace BlockchainSharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+
+    public class AccountState
+    {
+        private BigInteger balance;
+
+        public AccountState(BigInteger balance)
+        {
+            this.balance = balance;
+        }
+
+        public BigInteger Balance { get { return this.balance; } }
+    }
+}
