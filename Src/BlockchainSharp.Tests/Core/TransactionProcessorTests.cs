277b426ff69d0e834c86bb9541dfc183f5409c77
ajlopez <webmaster@ajlopez.com>
2016-04-23 10:23:57 -0300

    Create TransactionProcessro with initial account states

---
+++ Src/BlockchainSharp.Tests/Core/TransactionProcessorTests.cs
@@ -0,0 +1,21 @@
+﻿namespace BlockchainSharp.Tests.Core
+{
+    using System;
+    using BlockchainSharp.Core;
+    using BlockchainSharp.Tries;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TransactionProcessorTests
+    {
+        [TestMethod]
+        public void CreateWithAccountStates()
+        {
+            var states = new Trie<AccountState>();
+            var tp = new TransactionProcessor(states);
+
+            Assert.IsNotNull(tp.States);
+            Assert.AreSame(states, tp.States);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Core/TransactionProcessor.cs
@@ -0,0 +1,20 @@
+﻿namespace BlockchainSharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BlockchainSharp.Tries;
+
+    public class TransactionProcessor
+    {
+        private Trie<AccountState> states;
+
+        public TransactionProcessor(Trie<AccountState> states)
+        {
+            this.states = states;
+        }
+
+        public Trie<AccountState> States { get { return this.states; } }
+    }
+}
