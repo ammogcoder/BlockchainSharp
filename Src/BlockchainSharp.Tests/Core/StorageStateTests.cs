61d50c84aa9b1401d41321115749c114d6c20201
ajlopez <webmaster@ajlopez.com>
2016-04-30 10:17:25 -0300

    StorageState first tests

---
+++ Src/BlockchainSharp.Tests/Core/StorageStateTests.cs
@@ -0,0 +1,30 @@
+﻿namespace BlockchainSharp.Tests.Core
+{
+    using System;
+    using BlockchainSharp.Core;
+    using BlockchainSharp.Vm;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class StorageStateTests
+    {
+        [TestMethod]
+        public void GetZeroValue()
+        {
+            var state = new StorageState();
+
+            Assert.AreEqual(DataWord.Zero, state.Get(DataWord.One));
+        }
+
+        [TestMethod]
+        public void SetAndGetValue()
+        {
+            var state = new StorageState();
+
+            state.Put(DataWord.One, new DataWord(42));
+
+            Assert.AreEqual(DataWord.Zero, state.Get(DataWord.Zero));
+            Assert.AreEqual(new DataWord(42), state.Get(DataWord.One));
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Core/StorageState.cs
@@ -0,0 +1,26 @@
+﻿namespace BlockchainSharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using BlockchainSharp.Vm;
+
+    public class StorageState
+    {
+        private IDictionary<DataWord, DataWord> state = new Dictionary<DataWord, DataWord>();
+
+        public void Put(DataWord key, DataWord value)
+        {
+            this.state[key] = value;
+        }
+
+        public DataWord Get(DataWord key)
+        {
+            if (this.state.ContainsKey(key))
+                return this.state[key];
+
+            return DataWord.Zero;
+        }
+    }
+}
