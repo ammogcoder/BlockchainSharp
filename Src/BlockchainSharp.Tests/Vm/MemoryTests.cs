4f9870525384d2ec540674918b35f746a11a5fdf
ajlopez <webmaster@ajlopez.com>
2016-05-01 10:11:18 -0300

    VM Memory first test

---
+++ Src/BlockchainSharp.Tests/Vm/MemoryTests.cs
@@ -0,0 +1,20 @@
+﻿namespace BlockchainSharp.Tests.Vm
+{
+    using System;
+    using BlockchainSharp.Vm;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MemoryTests
+    {
+        [TestMethod]
+        public void GetUnitializedBytes()
+        {
+            var memory = new Memory();
+
+            Assert.AreEqual(0, memory.GetByte(DataWord.Zero));
+            Assert.AreEqual(0, memory.GetByte(DataWord.One));
+            Assert.AreEqual(0, memory.GetByte(DataWord.Two));
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Vm/Memory.cs
@@ -0,0 +1,15 @@
+﻿namespace BlockchainSharp.Vm
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Memory
+    {
+        public byte GetByte(DataWord address)
+        {
+            return 0;
+        }
+    }
+}
