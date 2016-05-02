1170a582dcf20cef37298d6cf7b0c5e03f285324
ajlopez <webmaster@ajlopez.com>
2016-05-02 08:58:52 -0300

    Memory set byte

--- Src/BlockchainSharp.Tests/Vm/MemoryTests.cs
+++ Src/BlockchainSharp.Tests/Vm/MemoryTests.cs
@@ -16,5 +16,17 @@
             Assert.AreEqual(0, memory.GetByte(DataWord.One));
             Assert.AreEqual(0, memory.GetByte(DataWord.Two));
         }
+
+        [TestMethod]
+        public void SetAndGetBytes()
+        {
+            var memory = new Memory();
+
+            memory.PutByte(DataWord.One, 0x12);
+
+            Assert.AreEqual(0, memory.GetByte(DataWord.Zero));
+            Assert.AreEqual(0x12, memory.GetByte(DataWord.One));
+            Assert.AreEqual(0, memory.GetByte(DataWord.Two));
+        }
     }
 }
--- Src/BlockchainSharp/Vm/Memory.cs
+++ Src/BlockchainSharp/Vm/Memory.cs
@@ -3,13 +3,27 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Numerics;
     using System.Text;
 
     public class Memory
     {
+        private byte[] bytes;
+
         public byte GetByte(DataWord address)
         {
+            if (this.bytes != null)
+                return this.bytes[(int)address.Value];
+
             return 0;
+        }
+
+        public void PutByte(DataWord address, byte value)
+        {
+            if (this.bytes == null)
+                this.bytes = new byte[1024];
+
+            this.bytes[(int)address.Value] = value;
         }
     }
 }
