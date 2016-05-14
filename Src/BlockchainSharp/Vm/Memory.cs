a3449f8cf69fda722d0b741207208efcbd023ae4
ajlopez <webmaster@ajlopez.com>
2016-05-14 09:26:31 -0300

    Memory internal refactor, using offset in block

--- Src/BlockchainSharp/Vm/Memory.cs
+++ Src/BlockchainSharp/Vm/Memory.cs
@@ -8,16 +8,18 @@
 
     public class Memory
     {
+        private BigInteger blocksize = new BigInteger(1024);
         private IDictionary<BigInteger, byte[]> blocks = new Dictionary<BigInteger, byte[]>();
 
         public DataWord GetDataWord(DataWord address)
         {
-            byte[] bytes = this.GetBlock(address);
+            int offset;
+            byte[] bytes = this.GetBlock(address, out offset);
 
             if (bytes != null) 
             {
                 var dbytes = new byte[32];
-                Array.Copy(bytes, (int)address.Value, dbytes, 0, 32);
+                Array.Copy(bytes, offset, dbytes, 0, 32);
                 return new DataWord(dbytes);
             }
 
@@ -26,40 +28,55 @@
 
         public void PutBytes(DataWord address, byte[] values)
         {
-            byte[] bytes = this.GetOrCreateBlock(address);
+            int offset;
+            byte[] bytes = this.GetOrCreateBlock(address, out offset);
 
-            Array.Copy(values, 0, bytes, (int)address.Value, values.Length);
+            Array.Copy(values, 0, bytes, offset, values.Length);
         }
 
         public byte GetByte(DataWord address)
         {
-            byte[] bytes = this.GetBlock(address);
+            int offset;
+            byte[] bytes = this.GetBlock(address, out offset);
 
             if (bytes != null)
-                return bytes[(int)address.Value];
+                return bytes[offset];
 
             return 0;
         }
 
         public void PutByte(DataWord address, byte value)
         {
-            byte[] bytes = this.GetOrCreateBlock(address);
+            int offset;
+            byte[] bytes = this.GetOrCreateBlock(address, out offset);
 
-            bytes[(int)address.Value] = value;
+            bytes[offset] = value;
         }
 
-        private byte[] GetBlock(DataWord address) 
+        private byte[] GetBlock(DataWord address, out int offset) 
         {
-            if (this.blocks.ContainsKey(BigInteger.Zero))
-                return this.blocks[BigInteger.Zero];
+            var value = address.Value;
+            BigInteger remainder = 0;
+            var div = BigInteger.DivRem(value, this.blocksize, out remainder);
+
+            offset = (int)remainder;
+            
+            if (this.blocks.ContainsKey(div))
+                return this.blocks[div];
 
             return null;
         }
 
-        private byte[] GetOrCreateBlock(DataWord address) 
+        private byte[] GetOrCreateBlock(DataWord address, out int offset) 
         {
-            if (this.blocks.ContainsKey(BigInteger.Zero))
-                return this.blocks[BigInteger.Zero];
+            var value = address.Value;
+            BigInteger remainder = 0;
+            var div = BigInteger.DivRem(value, this.blocksize, out remainder);
+
+            offset = (int)remainder;
+
+            if (this.blocks.ContainsKey(div))
+                return this.blocks[div];
 
             this.blocks[BigInteger.Zero] = new byte[1024];
 
