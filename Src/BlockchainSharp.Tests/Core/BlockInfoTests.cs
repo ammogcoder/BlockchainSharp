609a203c7698785b9edee881c64c151b2de634f2
ajlopez <webmaster@ajlopez.com>
2016-06-07 06:21:36 -0300

    BlockInfo simple creation test

---
+++ Src/BlockchainSharp.Tests/Core/BlockInfoTests.cs
@@ -0,0 +1,25 @@
+﻿namespace BlockchainSharp.Tests.Core
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BlockchainSharp.Core;
+
+    [TestClass]
+    public class BlockInfoTests
+    {
+        [TestMethod]
+        public void CreateBlockInfo()
+        {
+            Block block = new Block(0, null);
+            AccountsState state = new AccountsState();
+
+            BlockInfo binfo = new BlockInfo(block, state);
+
+            Assert.AreSame(block, binfo.Block);
+            Assert.AreSame(state, binfo.State);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Core/BlockInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace BlockchainSharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class BlockInfo
+    {
+        private Block block;
+        private AccountsState state;
+
+        public BlockInfo(Block block, AccountsState state)
+        {
+            this.block = block;
+            this.state = state;
+        }
+
+        public Block Block { get { return this.block; } }
+
+        public AccountsState State { get { return this.state; } } 
+    }
+}
