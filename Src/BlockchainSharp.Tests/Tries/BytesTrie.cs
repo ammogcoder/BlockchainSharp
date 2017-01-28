87562b855c825285a9a5898322fdb39505450509
ajlopez <webmaster@ajlopez.com>
2017-01-28 12:39:33 -0300

    BytesTrie from typed Trie, WIP

---
+++ Src/BlockchainSharp.Tests/Tries/BytesTrie.cs
@@ -0,0 +1,71 @@
+﻿namespace BlockchainSharp.Tests.Tries
+{
+    using System;
+    using BlockchainSharp.Tries;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BytesTrieTests
+    {
+        [TestMethod]
+        public void GetUnknowKey()
+        {
+            BytesTrie trie = new BytesTrie();
+
+            Assert.IsNull(trie.Get("012"));
+        }
+
+        [TestMethod]
+        public void PutAndGetKeyValue()
+        {
+            BytesTrie trie = new BytesTrie();
+
+            var trie2 = trie.Put("012", new byte[] { 1, 2, 3 });
+
+            Assert.IsNotNull(trie2);
+            Assert.AreNotSame(trie2, trie);
+            Assert.IsNull(trie.Get("012"));
+            Assert.IsTrue(Array.Equals(new byte[] { 1, 2, 3 }, trie2.Get("012")));
+        }
+
+        [TestMethod]
+        public void PutAndGetKeyValues()
+        {
+            BytesTrie trie = new BytesTrie();
+
+            var trie2 = trie.Put("012", new byte[] { 1, 2, 3 });
+            var trie3 = trie2.Put("abc", new byte[] { 4, 5, 6 });
+
+            Assert.IsNotNull(trie2);
+            Assert.AreNotSame(trie2, trie);
+            Assert.IsNull(trie.Get("012"));
+            Assert.IsNull(trie.Get("abc"));
+            Assert.AreEqual(new byte[] { 1, 2, 3 }, trie2.Get("012"));
+            Assert.IsNull(trie2.Get("abc"));
+
+            Assert.IsNotNull(trie3);
+            Assert.AreNotSame(trie3, trie2);
+            Assert.AreEqual(new byte[] { 1, 2, 3 }, trie3.Get("012"));
+            Assert.AreEqual(new byte[] { 4, 5, 6 }, trie3.Get("abc"));
+        }
+
+        [TestMethod]
+        public void ReplaceValue()
+        {
+            Trie<string> trie = new Trie<string>();
+
+            var trie2 = trie.Put("012", "foo");
+            var trie3 = trie2.Put("012", "bar");
+
+            Assert.IsNotNull(trie2);
+            Assert.AreNotSame(trie2, trie);
+            Assert.IsNull(trie.Get("012"));
+            Assert.AreEqual("foo", trie2.Get("012"));
+
+            Assert.IsNotNull(trie3);
+            Assert.AreNotSame(trie3, trie2);
+            Assert.AreEqual("bar", trie3.Get("012"));
+        }
+    }
+}
+
---
+++ Src/BlockchainSharp/Tries/BytesTrie.cs
@@ -0,0 +1,95 @@
+﻿namespace BlockchainSharp.Tries
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class BytesTrie
+    {
+        private BytesTrie[] leafs;
+        private byte[] value;
+
+        public BytesTrie() 
+        {
+        }
+
+        private BytesTrie(BytesTrie[] leafs)
+        {
+            this.leafs = leafs;
+        }
+
+        private BytesTrie(byte[] value, BytesTrie[] leafs)
+        {
+            this.value = value;
+            this.leafs = leafs;
+        }
+
+        public BytesTrie Put(string key, byte[] value)
+        {
+            return this.Put(key, 0, value);
+        }
+
+        public byte[] Get(string key)
+        {
+            return this.Get(key, 0);
+        }
+
+        private static int GetOffset(char ch)
+        {
+            if (ch >= '0' && ch <= '9')
+                return ch - '0';
+
+            return ch - 'a' + 10;
+        }
+
+        private byte[] Get(string key, int position)
+        {
+            if (position == key.Length)
+                return this.value;
+
+            var offset = GetOffset(key[position]);
+
+            if (this.leafs == null)
+                return null;
+
+            var trie = this.leafs[offset];
+
+            if (trie == null)
+                return null;
+
+            return trie.Get(key, position + 1);
+        }
+
+        private BytesTrie Put(string key, int position, byte[] value)
+        {
+            BytesTrie[] newleafs;
+
+            if (this.leafs != null)
+                newleafs = (BytesTrie[])this.leafs.Clone();
+            else
+                newleafs = null;
+
+            if (position == key.Length)
+                return new BytesTrie(value, newleafs);
+
+            int offset = GetOffset(key[position]);
+
+            if (this.leafs != null && this.leafs[offset] != null)
+            {
+                newleafs[offset] = this.leafs[offset].Put(key, position + 1, value);
+
+                return new BytesTrie(newleafs);
+            }
+
+            if (newleafs == null)
+                newleafs = new BytesTrie[16];
+
+            var newtrie = new BytesTrie();
+
+            newleafs[offset] = newtrie.Put(key, position + 1, value);
+
+            return new BytesTrie(newleafs);
+        }
+    }
+}
