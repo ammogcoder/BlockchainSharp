4c432195f01bdcc6f94c8d1ca5d04be282dc3b21
ajlopez <webmaster@ajlopez.com>
2017-06-24 13:46:24 -0300

    BigIntegerEncoder

---
+++ Src/BlockchainSharp.Tests/Encoding/BigIntegerTests.cs
@@ -0,0 +1,26 @@
+﻿namespace BlockchainSharp.Tests.Encoding
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BlockchainSharp.Encoding;
+    using System.Numerics;
+
+    [TestClass]
+    public class BigIntegerTests
+    {
+        [TestMethod]
+        public void EncodeDecodeBigIntegerOne()
+        {
+            BigIntegerEncoder encoder = new BigIntegerEncoder();
+
+            byte[] bytes = encoder.Encode(BigInteger.One);
+
+            Assert.IsNotNull(bytes);
+            Assert.AreNotEqual(0, bytes.Length);
+
+            BigInteger result = encoder.Decode(bytes);
+
+            Assert.AreEqual(BigInteger.One, result);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Encoding/BigIntegerEncoder.cs
@@ -0,0 +1,21 @@
+﻿namespace BlockchainSharp.Encoding
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+
+    public class BigIntegerEncoder
+    {
+        public byte[] Encode(BigInteger value)
+        {
+            return Rlp.Encode(value.ToByteArray());
+        }
+
+        public BigInteger Decode(byte[] bytes)
+        {
+            return new BigInteger(Rlp.Decode(bytes));
+        }
+    }
+}
