46f8c23a4090c7e4687a6b1a72478ff934935759
ajlopez <webmaster@ajlopez.com>
2017-06-24 13:36:03 -0300

    AccountEncoder

---
+++ Src/BlockchainSharp.Tests/Encoding/AddressEncoderTests.cs
@@ -0,0 +1,28 @@
+﻿namespace BlockchainSharp.Tests.Encoding
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BlockchainSharp.Core;
+    using BlockchainSharp.Encoding;
+
+    [TestClass]
+    public class AddressEncoderTests
+    {
+        [TestMethod]
+        public void EncodeAndDecodeAddress()
+        {
+            Address address = new Address();
+            AddressEncoder encoder = new AddressEncoder();
+
+            byte[] bytes = encoder.Encode(address);
+
+            Assert.IsNotNull(bytes);
+            Assert.AreNotEqual(0, bytes.Length);
+
+            Address result = encoder.Decode(bytes);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(address, result);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Encoding/AddressEncoder.cs
@@ -0,0 +1,23 @@
+﻿namespace BlockchainSharp.Encoding
+{
+    using BlockchainSharp.Core;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class AddressEncoder
+    {
+        public byte[] Encode(Address address)
+        {
+            return Rlp.Encode(address.Bytes);
+        }
+
+        public Address Decode(byte[] bytes)
+        {
+            byte[] addr = Rlp.Decode(bytes);
+
+            return new Address(addr);
+        }
+    }
+}
