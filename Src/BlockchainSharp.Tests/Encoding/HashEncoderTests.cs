7f725d01b8d9a3c987c759598102553ab0a0e4e8
ajlopez <webmaster@ajlopez.com>
2017-06-25 12:24:18 -0300

    Hash encoder

---
+++ Src/BlockchainSharp.Tests/Encoding/HashEncoderTests.cs
@@ -0,0 +1,28 @@
+﻿namespace BlockchainSharp.Tests.Encoding
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BlockchainSharp.Core;
+    using BlockchainSharp.Encoding;
+
+    [TestClass]
+    public class HashEncoderTests
+    {
+        [TestMethod]
+        public void EncodeAndDecodeHash()
+        {
+            Hash hash = new Hash();
+            HashEncoder encoder = new HashEncoder();
+
+            byte[] bytes = encoder.Encode(hash);
+
+            Assert.IsNotNull(bytes);
+            Assert.AreNotEqual(0, bytes.Length);
+
+            Hash result = encoder.Decode(bytes);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(hash, result);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Encoding/HashEncoder.cs
@@ -0,0 +1,23 @@
+﻿namespace BlockchainSharp.Encoding
+{
+    using BlockchainSharp.Core;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class HashEncoder
+    {
+        public byte[] Encode(Hash hash)
+        {
+            return Rlp.Encode(hash.Bytes);
+        }
+
+        public Hash Decode(byte[] bytes)
+        {
+            byte[] hash = Rlp.Decode(bytes);
+
+            return new Hash(hash);
+        }
+    }
+}
