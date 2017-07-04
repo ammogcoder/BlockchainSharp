0f10b20fe08f0a745ee5bb1191aa37fb08ff4695
ajlopez <webmaster@ajlopez.com>
2017-07-04 13:57:24 -0300

    Transaction encoder, WIP

---
+++ Src/BlockchainSharp.Tests/Encoding/TransactionEncoderTests.cs
@@ -0,0 +1,35 @@
+﻿namespace BlockchainSharp.Tests.Encoding
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BlockchainSharp.Encoding;
+    using BlockchainSharp.Core;
+    using System.Numerics;
+
+    [TestClass]
+    public class TransactionEncoderTests
+    {
+        [TestMethod]
+        public void EncodeDecodeTransaction()
+        {
+            TransactionEncoder encoder = new TransactionEncoder();
+            var sender = new Address();
+            var receiver = new Address();
+
+            Transaction transaction = new Transaction(sender, new BigInteger(100), receiver, new BigInteger(90));
+
+            var result = encoder.Decode(encoder.Encode(transaction));
+
+            Assert.IsNotNull(result);
+
+            Assert.IsNotNull(result.Receiver);
+            Assert.AreEqual(receiver, result.Receiver);
+
+            Assert.IsNotNull(result.Sender);
+            Assert.AreEqual(sender, result.Sender);
+
+            Assert.AreEqual(new BigInteger(100), result.SenderValue);
+            Assert.AreEqual(new BigInteger(90), result.ReceiverValue);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Encoding/TransactionEncoder.cs
@@ -0,0 +1,21 @@
+﻿namespace BlockchainSharp.Encoding
+{
+    using BlockchainSharp.Core;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class TransactionEncoder
+    {
+        public byte[] Encode(Transaction tx)
+        {
+            return null;
+        }
+
+        public Transaction Decode(byte[] bytes)
+        {
+            return null;
+        }
+    }
+}
