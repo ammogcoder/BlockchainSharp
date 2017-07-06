7864a3ecca9dce1330c07b67de69b8d78d0f49aa
ajlopez <webmaster@ajlopez.com>
2017-07-06 15:05:21 -0300

    Block Encoder

---
+++ Src/BlockchainSharp.Tests/Encoding/BlockEncoderTests.cs
@@ -0,0 +1,26 @@
+﻿namespace BlockchainSharp.Tests.Encoding
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BlockchainSharp.Encoding;
+    using BlockchainSharp.Core;
+    using System.Numerics;
+
+    [TestClass]
+    public class BlockEncoderTests
+    {
+        [TestMethod]
+        public void EncodeDecodeBlock()
+        {
+            BlockEncoder encoder = new BlockEncoder();
+            Hash hash = new Hash();
+            Block block = new Block(1, hash);
+
+            var result = encoder.Decode(encoder.Encode(block));
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Number);
+            Assert.AreEqual(hash, result.ParentHash);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Encoding/BlockEncoder.cs
@@ -0,0 +1,21 @@
+﻿namespace BlockchainSharp.Encoding
+{
+    using BlockchainSharp.Core;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class BlockEncoder
+    {
+        public byte[] Encode(Block block)
+        {
+            return null;
+        }
+
+        public Block Decode(byte[] bytes)
+        {
+            return null;
+        }
+    }
+}
