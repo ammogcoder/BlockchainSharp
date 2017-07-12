4d975bcc22638603001d5dddf87d147185bc25df
ajlopez <webmaster@ajlopez.com>
2017-07-12 10:13:03 -0300

    Block Header, first tests

---
+++ Src/BlockchainSharp.Tests/Core/BlockHeaderTests.cs
@@ -0,0 +1,32 @@
+﻿namespace BlockchainSharp.Tests.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+    using BlockchainSharp.Core;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BlockHeaderTests
+    {
+        [TestMethod]
+        public void CreateWithNumber()
+        {
+            BlockHeader header = new BlockHeader(42, null);
+
+            Assert.AreEqual(42, header.Number);
+        }
+
+        [TestMethod]
+        public void CreateWithNumberAndParentHash()
+        {
+            Hash parentHash = new Hash();
+            BlockHeader header = new BlockHeader(42, parentHash);
+
+            Assert.AreEqual(42, header.Number);
+            Assert.AreEqual(parentHash, header.ParentHash);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Core/BlockHeader.cs
@@ -0,0 +1,24 @@
+﻿namespace BlockchainSharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class BlockHeader
+    {
+        private long number;
+        private Hash hash;
+        private Hash parentHash;
+
+        public BlockHeader(long number, Hash parentHash)
+        {
+            this.number = number;
+            this.parentHash = parentHash;
+        }
+
+        public long Number { get { return this.number; } }
+
+        public Hash ParentHash { get { return this.parentHash; } }
+    }
+}
