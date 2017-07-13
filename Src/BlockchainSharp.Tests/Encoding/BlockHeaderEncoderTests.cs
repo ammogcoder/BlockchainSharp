17efa43a004dfbbba57894008e1c6c566a6bc7b1
ajlopez <webmaster@ajlopez.com>
2017-07-13 11:25:59 -0300

    BlockHeaderEncoder

---
+++ Src/BlockchainSharp.Tests/Encoding/BlockHeaderEncoderTests.cs
@@ -0,0 +1,26 @@
+﻿namespace BlockchainSharp.Tests.Encoding
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BlockchainSharp.Encoding;
+    using BlockchainSharp.Core;
+    using System.Numerics;
+
+    [TestClass]
+    public class BlockHeaderEncoderTests
+    {
+        [TestMethod]
+        public void EncodeDecodeBlockHeader()
+        {
+            BlockHeaderEncoder encoder = new BlockHeaderEncoder();
+            Hash hash = new Hash();
+            BlockHeader header = new BlockHeader(1, hash);
+
+            var result = encoder.Decode(encoder.Encode(header));
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Number);
+            Assert.AreEqual(hash, result.ParentHash);
+        }
+    }
+}
---
+++ Src/BlockchainSharp/Encoding/BlockHeaderEncoder.cs
@@ -0,0 +1,36 @@
+﻿namespace BlockchainSharp.Encoding
+{
+    using BlockchainSharp.Core;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+
+    public class BlockHeaderEncoder
+    {
+        private static BigIntegerEncoder bigIntegerEncoder = new BigIntegerEncoder();
+        private static HashEncoder hashEncoder = new HashEncoder();
+        private static BlockHeaderEncoder instance = new BlockHeaderEncoder();
+
+        public static BlockHeaderEncoder Instance { get { return instance; } }
+
+        public byte[] Encode(BlockHeader header)
+        {
+            byte[] number = bigIntegerEncoder.Encode(new BigInteger(header.Number));
+            byte[] hash = hashEncoder.Encode(header.ParentHash);
+
+            return Rlp.EncodeList(number, hash);
+        }
+
+        public BlockHeader Decode(byte[] bytes)
+        {
+            IList<byte[]> list = Rlp.DecodeList(bytes);
+
+            long number = (long)bigIntegerEncoder.Decode(list[0]);
+            Hash hash = hashEncoder.Decode(list[1]);
+
+            return new BlockHeader(number, hash);
+        }
+    }
+}
