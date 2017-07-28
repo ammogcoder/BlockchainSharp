8109c7893f9c73df5ac8785b1214f245407f0b7d
ajlopez <webmaster@ajlopez.com>
2017-07-28 13:01:22 -0300

    Block with transaction encoding, WIP

--- Src/BlockchainSharp.Tests/Encoding/BlockEncoderTests.cs
+++ Src/BlockchainSharp.Tests/Encoding/BlockEncoderTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace BlockchainSharp.Tests.Encoding
 {
     using System;
+    using System.Linq;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using BlockchainSharp.Encoding;
     using BlockchainSharp.Core;
@@ -21,6 +22,25 @@
             Assert.IsNotNull(result);
             Assert.AreEqual(1, result.Number);
             Assert.AreEqual(hash, result.ParentHash);
+        }
+
+        [TestMethod]
+        public void EncodeDecodeBlockWithTransaction()
+        {
+            BlockEncoder encoder = new BlockEncoder();
+            Address from = new Address();
+            Address to = new Address();
+            Transaction transaction = new Transaction(from, to, new BigInteger(2));
+
+            Block original = new Block(0, null, new Transaction[] { transaction });
+            Block block = encoder.Decode(encoder.Encode(original));
+
+            Assert.IsNotNull(block.Transactions);
+            Assert.AreEqual(1, block.Transactions.Count);
+
+            Transaction result = block.Transactions.First();
+
+            Assert.AreEqual(new BigInteger(2), result.Value);
         }
     }
 }
